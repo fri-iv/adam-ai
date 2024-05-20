b9d4e151b6b97567b4f6125cd96725a295fedcd0
ilgeez <xameleonicus@gmail.com>
2024-05-20 15:24:47 +0300

    update logging

--- src/main/java/com/github/novicezk/midjourney/bot/events/EventsManager.java
+++ src/main/java/com/github/novicezk/midjourney/bot/events/EventsManager.java
@@ -1,9 +1,11 @@
 package com.github.novicezk.midjourney.bot.events;
 
+import com.github.novicezk.midjourney.bot.AdamBotInitializer;
 import com.github.novicezk.midjourney.bot.events.model.ButtonEventData;
 import com.github.novicezk.midjourney.bot.events.model.CommandEventData;
 import com.github.novicezk.midjourney.bot.events.model.ErrorEventData;
 import com.github.novicezk.midjourney.bot.events.model.LeaveEventData;
+import com.github.novicezk.midjourney.bot.utils.ColorUtil;
 import com.github.novicezk.midjourney.bot.utils.Config;
 import com.github.novicezk.midjourney.bot.utils.EmbedUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +16,8 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
 
 @Slf4j
 public class EventsManager {
@@ -40,7 +44,7 @@ public class EventsManager {
 
     public static void onMemberLeave(GuildMemberRemoveEvent event, String reason) {
         MixpanelManager.trackEvent(new LeaveEventData(event, reason));
-        sendLogToDiscord(event.getGuild(), event.getUser().getId(), event.getUser().getName(), reason);
+        sendLogToDiscord(event.getGuild(), event.getUser().getId(), event.getUser().getName(), reason, ColorUtil.getWarningColor());
     }
 
     public static void onErrorEvent(String userId, String failReason) {
@@ -48,15 +52,39 @@ public class EventsManager {
     }
 
     public static void onMidjourneyFailure(String failReason) {
-        log.info("Midjourney task finished FAILURE - {}", failReason);
+        String errorMessage = "Midjourney task finished FAILURE - " + failReason;
+        log.info(errorMessage);
+
+        Guild guild = AdamBotInitializer.getApiInstance().getGuildById(Config.getGuildId());
+        sendLogToDiscord(guild, null, null, errorMessage, ColorUtil.getErrorColor());
     }
 
     private static void sendLogToDiscord(@Nullable Guild guild, String userId, String username, String text) {
+        sendLogToDiscord(guild, userId, username, text, ColorUtil.getDefaultColor());
+    }
+
+    private static void sendLogToDiscord(
+            @Nullable Guild guild,
+            @Nullable String userId,
+            @Nullable String username,
+            String text,
+            Color color
+    ) {
         if (guild == null || guild.getTextChannelById(Config.getLogsChannel()) == null) {
             return;
         }
 
+        String postText = text;
+        if (userId != null && username != null) {
+            postText = String.format("%s — <@%s>, %s", text, userId, username);
+        }
+
         TextChannel logChannel = guild.getTextChannelById(Config.getLogsChannel());
-        logChannel.sendMessageEmbeds(EmbedUtil.createEmbed(String.format("%s — <@%s>, %s", text, userId, username))).queue();
+        logChannel.sendMessageEmbeds(EmbedUtil.createEmbed(
+                null,
+                postText,
+                Config.getAppVersion(),
+                color
+        )).queue();
     }
 }
